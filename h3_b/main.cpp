dc3c2dad695f89d542b0f095444af49e2c3dbd0b
Ard3J <arttujamsa@gmail.com>
2025-11-21 14:27:47 +0200

    Ääkköset

--- h3_b/main.cpp
+++ h3_b/main.cpp
@@ -1,11 +1,16 @@
 #include "animal.h"
 #include "dog.h"
 #include <iostream>
+#include <windows.h>
+
 
 using namespace std;
 
 int main()
 {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     Animal objAnimal;
     Dog objDog;
     objAnimal.callOut();
